6e65ad7201ba3c06e8b858e526c851492c71d50d
nizzleYo <nms@netcompany.com>
2023-04-19 12:10:46 +0200

    Add request to fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+import requests as rq
 
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -17,3 +18,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
